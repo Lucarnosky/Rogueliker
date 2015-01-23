be33a1ddfaebb8d2aa01263588a9de9ea4487f7e
Lucarnosky <lucarnosky@gmail.com>
2015-01-23 18:34:07 +0100

    Added level editor handler

---
+++ src/com/styxsailors/rogue/handler/EditorHandler.java
@@ -0,0 +1,23 @@
+package com.styxsailors.rogue.handler;
+
+import java.awt.Graphics2D;
+
+import com.styxsailors.rogue.utils.Global;
+
+public class EditorHandler {
+	
+	Global global;
+	
+	public EditorHandler(Global global){
+		this.global = global;
+	}
+	
+	public void tick(){
+		
+	}
+	
+	public void render(Graphics2D g){
+		
+	}
+
+}
