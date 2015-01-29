5cf085c38decca4e6145e686249ee6c01e9ef5fd
Lucarnosky <lucarnosky@gmail.com>
2015-01-29 16:05:26 +0100

    Fixed and full working version

--- src/com/styxsailors/rogue/editor/menu/voices/ExportLevelVoice.java
+++ src/com/styxsailors/rogue/editor/menu/voices/ExportLevelVoice.java
@@ -28,41 +28,16 @@ public class ExportLevelVoice extends SubMenuButton{
 		ArrayList<Tile> gridToExport = new ArrayList<>(global.editor.getGrid());
 		try {
 			PrintWriter writer  = new PrintWriter("res/levels/"+ levelName +".lvl", "UTF-8");
-			writer.write(levelName + "\n");
+			writer.write(levelName+"\n");
 			for(int i = 0; i < gridToExport.size(); i ++){
-				//writer.append("("+gridToExport.get(i).x+","+gridToExport.get(i).y+","+gridToExport.get(i).getId()+")\n");
+				writer.append("("+gridToExport.get(i).x+","+gridToExport.get(i).y+","+gridToExport.get(i).getId()+")\n");
 			}
+			writer.close();
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			
 			e.printStackTrace();
 		};
 		
 	}
-	
-//	public void exportLevel(String levelName){
-//		PrintWriter writer;
-//		try {
-//			writer = new PrintWriter("res/levels/"+ levelName +".lvl", "UTF-8");
-//			
-//			if(levelName != null){ // If not specified a next Level Name, call itself
-//				writer.write(nextLevelName + "\n");
-//				writer.append(String.valueOf(width)+ "\n");
-//			}else{
-//				writer.write(levelName + "\n");
-//				writer.append(String.valueOf(width)+ "\n");
-//			}
-//			writer.append(String.valueOf(height) + "\n");
-//			for(int i = 0; i < mainTiles.size(); i ++){
-//				writer.append(String.valueOf(mainTiles.get(i).getX()) + "\n");
-//				writer.append(String.valueOf(mainTiles.get(i).getY()) + "\n");
-//				writer.append(String.valueOf(mainTiles.get(i).selectedLayer)+ "\n");
-//				writer.append(String.valueOf(mainTiles.get(i).index) + "\n");
-//			}
-//			writer.close();
-//		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}
 
 }
